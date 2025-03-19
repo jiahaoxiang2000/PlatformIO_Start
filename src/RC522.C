4750b63114ab5ff3ea3b474de6577b823094f11d
isomo <jiahaoxiang2000@gmail.com>
2025-03-19 20:27:55 +0800

    Add MFRC522 header file with function declarations and definitions

---
+++ src/RC522.C
@@ -0,0 +1,648 @@
+#ifdef __C51__
+#include <intrins.h>
+#else
+// Define _nop_ function for non-8051 platforms
+#define _nop_() __asm__("nop")
+#endif
+
+#include "main.h"
+#include "mfrc522.h"
+
+#define MAXRLEN 18
+
+/////////////////////////////////////////////////////////////////////
+// Function: Card detection
+// Parameter description: req_code[IN]: Detection mode
+//                 0x52 = Detect all cards complying with 14443A standard
+//                 0x26 = Detect cards that are not in HALT state
+//           pTagType[OUT]: Card type code
+//                 0x4400 = Mifare_UltraLight
+//                 0x0400 = Mifare_One(S50)
+//                 0x0200 = Mifare_One(S70)
+//                 0x0800 = Mifare_Pro(X)
+//                 0x4403 = Mifare_DESFire
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdRequest(unsigned char req_code, unsigned char *pTagType)
+{
+    char status;
+    unsigned int unLen;
+    unsigned char ucComMF522Buf[MAXRLEN];
+
+    ClearBitMask(Status2Reg, 0x08);
+    WriteRawRC(BitFramingReg, 0x07);
+    SetBitMask(TxControlReg, 0x03);
+
+    ucComMF522Buf[0] = req_code;
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 1, ucComMF522Buf, &unLen);
+
+    if ((status == MI_OK) && (unLen == 0x10))
+    {
+        *pTagType = ucComMF522Buf[0];
+        *(pTagType + 1) = ucComMF522Buf[1];
+    }
+    else
+    {
+        status = MI_ERR;
+    }
+
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Anti-collision
+// Parameter description: pSnr[OUT]: Card serial number, 4 bytes
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdAnticoll(unsigned char *pSnr)
+{
+    char status;
+    unsigned char i, snr_check = 0;
+    unsigned int unLen;
+    unsigned char ucComMF522Buf[MAXRLEN];
+
+    ClearBitMask(Status2Reg, 0x08);
+    WriteRawRC(BitFramingReg, 0x00);
+    ClearBitMask(CollReg, 0x80);
+
+    ucComMF522Buf[0] = PICC_ANTICOLL1;
+    ucComMF522Buf[1] = 0x20;
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 2, ucComMF522Buf, &unLen);
+
+    if (status == MI_OK)
+    {
+        for (i = 0; i < 4; i++)
+        {
+            *(pSnr + i) = ucComMF522Buf[i];
+            snr_check ^= ucComMF522Buf[i];
+        }
+        if (snr_check != ucComMF522Buf[i])
+        {
+            status = MI_ERR;
+        }
+    }
+
+    SetBitMask(CollReg, 0x80);
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Select card
+// Parameter description: pSnr[IN]: Card serial number, 4 bytes
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdSelect(unsigned char *pSnr)
+{
+    char status;
+    unsigned char i;
+    unsigned int unLen;
+    unsigned char ucComMF522Buf[MAXRLEN];
+
+    ucComMF522Buf[0] = PICC_ANTICOLL1;
+    ucComMF522Buf[1] = 0x70;
+    ucComMF522Buf[6] = 0;
+    for (i = 0; i < 4; i++)
+    {
+        ucComMF522Buf[i + 2] = *(pSnr + i);
+        ucComMF522Buf[6] ^= *(pSnr + i);
+    }
+    CalulateCRC(ucComMF522Buf, 7, &ucComMF522Buf[7]);
+
+    ClearBitMask(Status2Reg, 0x08);
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 9, ucComMF522Buf, &unLen);
+
+    if ((status == MI_OK) && (unLen == 0x18))
+    {
+        status = MI_OK;
+    }
+    else
+    {
+        status = MI_ERR;
+    }
+
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Authenticate card key
+// Parameter description: auth_mode[IN]: Authentication mode
+//                  0x60 = Authenticate A key
+//                  0x61 = Authenticate B key
+//           addr[IN]: Block address
+//           pKey[IN]: Key data
+//           pSnr[IN]: Card serial number, 4 bytes
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdAuthState(unsigned char auth_mode, unsigned char addr, unsigned char *pKey, unsigned char *pSnr)
+{
+    char status;
+    unsigned int unLen;
+    unsigned char i, ucComMF522Buf[MAXRLEN];
+
+    ucComMF522Buf[0] = auth_mode;
+    ucComMF522Buf[1] = addr;
+    for (i = 0; i < 6; i++)
+    {
+        ucComMF522Buf[i + 2] = *(pKey + i);
+    }
+    for (i = 0; i < 6; i++)
+    {
+        ucComMF522Buf[i + 8] = *(pSnr + i);
+    }
+    //   memcpy(&ucComMF522Buf[2], pKey, 6);
+    //   memcpy(&ucComMF522Buf[8], pSnr, 4);
+
+    status = PcdComMF522(PCD_AUTHENT, ucComMF522Buf, 12, ucComMF522Buf, &unLen);
+    if ((status != MI_OK) || (!(ReadRawRC(Status2Reg) & 0x08)))
+    {
+        status = MI_ERR;
+    }
+
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Read one block from M1 card
+// Parameter description: addr[IN]: Block address
+//           pData[OUT]: Read data buffer, 16 bytes
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdRead(unsigned char addr, unsigned char *pData)
+{
+    char status;
+    unsigned int unLen;
+    unsigned char i, ucComMF522Buf[MAXRLEN];
+
+    ucComMF522Buf[0] = PICC_READ;
+    ucComMF522Buf[1] = addr;
+    CalulateCRC(ucComMF522Buf, 2, &ucComMF522Buf[2]);
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, &unLen);
+    if ((status == MI_OK) && (unLen == 0x90))
+    //   {   memcpy(pData, ucComMF522Buf, 16);   }
+    {
+        for (i = 0; i < 16; i++)
+        {
+            *(pData + i) = ucComMF522Buf[i];
+        }
+    }
+    else
+    {
+        status = MI_ERR;
+    }
+
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Write data to M1 card block
+// Parameter description: addr[IN]: Block address
+//           pData[IN]: Data to write, 16 bytes
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdWrite(unsigned char addr, unsigned char *pData)
+{
+    char status;
+    unsigned int unLen;
+    unsigned char i, ucComMF522Buf[MAXRLEN];
+
+    ucComMF522Buf[0] = PICC_WRITE;
+    ucComMF522Buf[1] = addr;
+    CalulateCRC(ucComMF522Buf, 2, &ucComMF522Buf[2]);
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, &unLen);
+
+    if ((status != MI_OK) || (unLen != 4) || ((ucComMF522Buf[0] & 0x0F) != 0x0A))
+    {
+        status = MI_ERR;
+    }
+
+    if (status == MI_OK)
+    {
+        // memcpy(ucComMF522Buf, pData, 16);
+        for (i = 0; i < 16; i++)
+        {
+            ucComMF522Buf[i] = *(pData + i);
+        }
+        CalulateCRC(ucComMF522Buf, 16, &ucComMF522Buf[16]);
+
+        status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 18, ucComMF522Buf, &unLen);
+        if ((status != MI_OK) || (unLen != 4) || ((ucComMF522Buf[0] & 0x0F) != 0x0A))
+        {
+            status = MI_ERR;
+        }
+    }
+
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Debit or charge value
+// Parameter description: dd_mode[IN]: Operation mode
+//                0xC0 = Debit
+//                0xC1 = Charge
+//           addr[IN]: Value block address
+//           pValue[IN]: 4-byte value (with LSB first)
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdValue(unsigned char dd_mode, unsigned char addr, unsigned char *pValue)
+{
+    char status;
+    unsigned int unLen;
+    unsigned char i, ucComMF522Buf[MAXRLEN];
+
+    ucComMF522Buf[0] = dd_mode;
+    ucComMF522Buf[1] = addr;
+    CalulateCRC(ucComMF522Buf, 2, &ucComMF522Buf[2]);
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, &unLen);
+
+    if ((status != MI_OK) || (unLen != 4) || ((ucComMF522Buf[0] & 0x0F) != 0x0A))
+    {
+        status = MI_ERR;
+    }
+
+    if (status == MI_OK)
+    {
+        // memcpy(ucComMF522Buf, pValue, 4);
+        for (i = 0; i < 16; i++)
+        {
+            ucComMF522Buf[i] = *(pValue + i);
+        }
+        CalulateCRC(ucComMF522Buf, 4, &ucComMF522Buf[4]);
+        unLen = 0;
+        status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 6, ucComMF522Buf, &unLen);
+        if (status != MI_ERR)
+        {
+            status = MI_OK;
+        }
+    }
+
+    if (status == MI_OK)
+    {
+        ucComMF522Buf[0] = PICC_TRANSFER;
+        ucComMF522Buf[1] = addr;
+        CalulateCRC(ucComMF522Buf, 2, &ucComMF522Buf[2]);
+
+        status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, &unLen);
+
+        if ((status != MI_OK) || (unLen != 4) || ((ucComMF522Buf[0] & 0x0F) != 0x0A))
+        {
+            status = MI_ERR;
+        }
+    }
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Transfer value between blocks
+// Parameter description: sourceaddr[IN]: Source address
+//           goaladdr[IN]: Destination address
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdBakValue(unsigned char sourceaddr, unsigned char goaladdr)
+{
+    char status;
+    unsigned int unLen;
+    unsigned char ucComMF522Buf[MAXRLEN];
+
+    ucComMF522Buf[0] = PICC_RESTORE;
+    ucComMF522Buf[1] = sourceaddr;
+    CalulateCRC(ucComMF522Buf, 2, &ucComMF522Buf[2]);
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, &unLen);
+
+    if ((status != MI_OK) || (unLen != 4) || ((ucComMF522Buf[0] & 0x0F) != 0x0A))
+    {
+        status = MI_ERR;
+    }
+
+    if (status == MI_OK)
+    {
+        ucComMF522Buf[0] = 0;
+        ucComMF522Buf[1] = 0;
+        ucComMF522Buf[2] = 0;
+        ucComMF522Buf[3] = 0;
+        CalulateCRC(ucComMF522Buf, 4, &ucComMF522Buf[4]);
+
+        status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 6, ucComMF522Buf, &unLen);
+        if (status != MI_ERR)
+        {
+            status = MI_OK;
+        }
+    }
+
+    if (status != MI_OK)
+    {
+        return MI_ERR;
+    }
+
+    ucComMF522Buf[0] = PICC_TRANSFER;
+    ucComMF522Buf[1] = goaladdr;
+
+    CalulateCRC(ucComMF522Buf, 2, &ucComMF522Buf[2]);
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, &unLen);
+
+    if ((status != MI_OK) || (unLen != 4) || ((ucComMF522Buf[0] & 0x0F) != 0x0A))
+    {
+        status = MI_ERR;
+    }
+
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Put card in HALT state
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdHalt(void)
+{
+    char status;
+    unsigned int unLen;
+    unsigned char ucComMF522Buf[MAXRLEN];
+
+    ucComMF522Buf[0] = PICC_HALT;
+    ucComMF522Buf[1] = 0;
+    CalulateCRC(ucComMF522Buf, 2, &ucComMF522Buf[2]);
+
+    status = PcdComMF522(PCD_TRANSCEIVE, ucComMF522Buf, 4, ucComMF522Buf, &unLen);
+
+    return MI_OK;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Calculate CRC16 with MF522
+/////////////////////////////////////////////////////////////////////
+void CalulateCRC(unsigned char *pIndata, unsigned char len, unsigned char *pOutData)
+{
+    unsigned char i, n;
+    ClearBitMask(DivIrqReg, 0x04);
+    WriteRawRC(CommandReg, PCD_IDLE);
+    SetBitMask(FIFOLevelReg, 0x80);
+    for (i = 0; i < len; i++)
+    {
+        WriteRawRC(FIFODataReg, *(pIndata + i));
+    }
+    WriteRawRC(CommandReg, PCD_CALCCRC);
+    i = 0xFF;
+    do
+    {
+        n = ReadRawRC(DivIrqReg);
+        i--;
+    } while ((i != 0) && !(n & 0x04));
+    pOutData[0] = ReadRawRC(CRCResultRegL);
+    pOutData[1] = ReadRawRC(CRCResultRegM);
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Reset RC522
+// Return: Success returns MI_OK
+/////////////////////////////////////////////////////////////////////
+char PcdReset(void)
+{
+    // unsigned char i;
+    MF522_RST = 1;
+
+    _nop_();
+
+    MF522_RST = 0;
+
+    _nop_();
+
+    MF522_RST = 1;
+
+    _nop_();
+
+    WriteRawRC(CommandReg, PCD_RESETPHASE);
+
+    _nop_();
+
+    WriteRawRC(ModeReg, 0x3D); // CRC initial value for Mifare communication 0x6363
+    WriteRawRC(TReloadRegL, 30);
+    WriteRawRC(TReloadRegH, 0);
+    WriteRawRC(TModeReg, 0x8D);
+    WriteRawRC(TPrescalerReg, 0x3E);
+    WriteRawRC(TxAutoReg, 0x40);
+    return MI_OK;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Read RC632 register
+// Parameter description: Address[IN]: Register address
+// Return: Read value
+/////////////////////////////////////////////////////////////////////
+unsigned char ReadRawRC(unsigned char Address)
+{
+    unsigned char i, ucAddr;
+    unsigned char ucResult = 0;
+
+    MF522_SCK = 0;
+    MF522_NSS = 0;
+    ucAddr = ((Address << 1) & 0x7E) | 0x80;
+
+    for (i = 8; i > 0; i--)
+    {
+        MF522_SI = ((ucAddr & 0x80) == 0x80);
+        MF522_SCK = 1;
+        ucAddr <<= 1;
+        MF522_SCK = 0;
+    }
+
+    for (i = 8; i > 0; i--)
+    {
+        MF522_SCK = 1;
+        ucResult <<= 1;
+        ucResult |= (bit)MF522_SO;
+        MF522_SCK = 0;
+    }
+
+    MF522_NSS = 1;
+    MF522_SCK = 1;
+    return ucResult;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Write RC632 register
+// Parameter description: Address[IN]: Register address
+//           value[IN]: Value to write
+/////////////////////////////////////////////////////////////////////
+void WriteRawRC(unsigned char Address, unsigned char value)
+{
+    unsigned char i, ucAddr;
+
+    MF522_SCK = 0;
+    MF522_NSS = 0;
+    ucAddr = ((Address << 1) & 0x7E);
+
+    for (i = 8; i > 0; i--)
+    {
+        MF522_SI = ((ucAddr & 0x80) == 0x80);
+        MF522_SCK = 1;
+        ucAddr <<= 1;
+        MF522_SCK = 0;
+    }
+
+    for (i = 8; i > 0; i--)
+    {
+        MF522_SI = ((value & 0x80) == 0x80);
+        MF522_SCK = 1;
+        value <<= 1;
+        MF522_SCK = 0;
+    }
+    MF522_NSS = 1;
+    MF522_SCK = 1;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Set bits in RC522 register
+// Parameter description: reg[IN]: Register address
+//           mask[IN]: Bit mask value
+/////////////////////////////////////////////////////////////////////
+void SetBitMask(unsigned char reg, unsigned char mask)
+{
+    char tmp = 0x0;
+    tmp = ReadRawRC(reg);
+    WriteRawRC(reg, tmp | mask); // set bit mask
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Clear bits in RC522 register
+// Parameter description: reg[IN]: Register address
+//           mask[IN]: Bit mask value
+/////////////////////////////////////////////////////////////////////
+void ClearBitMask(unsigned char reg, unsigned char mask)
+{
+    char tmp = 0x0;
+    tmp = ReadRawRC(reg);
+    WriteRawRC(reg, tmp & ~mask); // clear bit mask
+}
+
+/////////////////////////////////////////////////////////////////////
+// Function: Communicate with ISO14443 card via RC522
+// Parameter description: Command[IN]: RC522 command
+//           pInData[IN]: Data to send to card via RC522
+//           InLenByte[IN]: Length of input data in bytes
+//           pOutData[OUT]: Data received from card
+//           *pOutLenBit[OUT]: Length of output data in bits
+/////////////////////////////////////////////////////////////////////
+char PcdComMF522(unsigned char Command,
+                 unsigned char *pInData,
+                 unsigned char InLenByte,
+                 unsigned char *pOutData,
+                 unsigned int *pOutLenBit)
+{
+    char status = MI_ERR;
+    unsigned char irqEn = 0x00;
+    unsigned char waitFor = 0x00;
+    unsigned char lastBits;
+    unsigned char n;
+    unsigned int i;
+    switch (Command)
+    {
+    case PCD_AUTHENT:
+        irqEn = 0x12;
+        waitFor = 0x10;
+        break;
+    case PCD_TRANSCEIVE:
+        irqEn = 0x77;
+        waitFor = 0x30;
+        break;
+    default:
+        break;
+    }
+
+    WriteRawRC(ComIEnReg, irqEn | 0x80);
+    ClearBitMask(ComIrqReg, 0x80);
+    WriteRawRC(CommandReg, PCD_IDLE);
+    SetBitMask(FIFOLevelReg, 0x80);
+
+    for (i = 0; i < InLenByte; i++)
+    {
+        WriteRawRC(FIFODataReg, pInData[i]);
+    }
+    WriteRawRC(CommandReg, Command);
+
+    if (Command == PCD_TRANSCEIVE)
+    {
+        SetBitMask(BitFramingReg, 0x80);
+    }
+
+    i = 600; // The maximum waiting time for M1 card is 25ms
+    do
+    {
+        n = ReadRawRC(ComIrqReg);
+        i--;
+    } while ((i != 0) && !(n & 0x01) && !(n & waitFor));
+    ClearBitMask(BitFramingReg, 0x80);
+
+    if (i != 0)
+    {
+        if (!(ReadRawRC(ErrorReg) & 0x1B))
+        {
+            status = MI_OK;
+            if (n & irqEn & 0x01)
+            {
+                status = MI_NOTAGERR;
+            }
+            if (Command == PCD_TRANSCEIVE)
+            {
+                n = ReadRawRC(FIFOLevelReg);
+                lastBits = ReadRawRC(ControlReg) & 0x07;
+                if (lastBits)
+                {
+                    *pOutLenBit = (n - 1) * 8 + lastBits;
+                }
+                else
+                {
+                    *pOutLenBit = n * 8;
+                }
+                if (n == 0)
+                {
+                    n = 1;
+                }
+                if (n > MAXRLEN)
+                {
+                    n = MAXRLEN;
+                }
+                for (i = 0; i < n; i++)
+                {
+                    pOutData[i] = ReadRawRC(FIFODataReg);
+                }
+            }
+        }
+        else
+        {
+            status = MI_ERR;
+        }
+    }
+
+    SetBitMask(ControlReg, 0x80); // stop timer now
+    WriteRawRC(CommandReg, PCD_IDLE);
+    return status;
+}
+
+/////////////////////////////////////////////////////////////////////
+// Turn on antenna
+// Should delay 1ms after turning on/off antenna
+/////////////////////////////////////////////////////////////////////
+void PcdAntennaOn()
+{
+    unsigned char i;
+    i = ReadRawRC(TxControlReg);
+    if (!(i & 0x03))
+    {
+        SetBitMask(TxControlReg, 0x03);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////
+// Turn off antenna
+/////////////////////////////////////////////////////////////////////
+void PcdAntennaOff()
+{
+    ClearBitMask(TxControlReg, 0x03);
+}
