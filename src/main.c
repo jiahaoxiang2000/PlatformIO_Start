01824964904c2ab1b2f51d3d480a86b63c052841
isomo <jiahaoxiang2000@gmail.com>
2025-03-19 20:40:21 +0800

    Add UART1 initialization and redirect printf output to UART

--- src/main.c
+++ src/main.c
@@ -1,15 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 void LED_Init();
+void UART1_Init();
+
+UART_HandleTypeDef huart1;
 
 int main(void)
 {
     HAL_Init();
     LED_Init();
+    UART1_Init();
+
+    printf("UART1 Initialized - Hello World\r\n");
 
     while (1)
     {
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+        printf("LED Toggle\r\n");
         HAL_Delay(1000);
     }
 }
@@ -23,6 +31,45 @@ void LED_Init()
     GPIO_InitStruct.Pull = GPIO_PULLDOWN;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
     HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+}
+
+void UART1_Init()
+{
+    UART1_GPIO_CLK_ENABLE();
+    UART1_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    // Configure UART1 TX pin
+    GPIO_InitStruct.Pin = UART1_TX_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(UART1_GPIO_PORT, &GPIO_InitStruct);
+
+    // Configure UART1 RX pin
+    GPIO_InitStruct.Pin = UART1_RX_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    HAL_GPIO_Init(UART1_GPIO_PORT, &GPIO_InitStruct);
+
+    // Configure UART1
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200; 
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    HAL_UART_Init(&huart1);
+}
+
+// Redirect printf to UART1
+int _write(int file, char *ptr, int len)
+{
+    HAL_UART_Transmit(&huart1, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+    return len;
 }
 
 void SysTick_Handler(void)
